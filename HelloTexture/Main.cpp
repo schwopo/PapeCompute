bc7e0c4de0acee0b2153c6915c810963a64242bb
Manuel Lofski <manuel.lofski@stud.uni-due.de>
2023-02-18 13:47:44 +0100

    Renamed project

--- HelloTexture/Main.cpp
+++ HelloTexture/Main.cpp
@@ -10,11 +10,11 @@
 //*********************************************************
 
 #include "stdafx.h"
-#include "D3D12HelloTexture.h"
+#include "PapeCompute.h"
 
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-    D3D12HelloTexture sample(1280, 720, L"D3D12 Hello Texture");
+    CPapeCompute sample(1280, 720, L"Pape Compute Edition 2023 (All rights reserved)");
     return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
